e3d5778906a09bba7b8fadda6dfef3e7a664f9c7
Marcus <marcusfish2004@gmail.com>
2024-02-05 18:56:49 +0000

    Added the performance rating function

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,14 +1,25 @@
+let goalsOrAssits = 0;
+
 function goalsScored() {
     let scoreMessage = ['You will score', 'You will not score but you will assit', 'You will not score'];
     let randomScoreMessage = Math.floor(Math.random() * 3);
 
-    let goalsOrAssits = Math.floor((Math.random() * 4) + 1);
+    goalsOrAssits = Math.floor((Math.random() * 4) + 1);
 
     if (randomScoreMessage !== 2) {
         return `${scoreMessage[randomScoreMessage]} ${goalsOrAssits}`;
     } else {
         return scoreMessage[randomScoreMessage];
     }
-}
+};
 
 console.log(goalsScored());
+
+function matchPerformance() {
+    let performanceReview = ['Abysmal', 'Poor', 'Mediocre', 'Below-Average', 'Average', 'Decent', 'Good', 'Impressive', 'Outstanding', 'Exceptional'];
+    let randomPerformance = Math.floor(Math.random() * 10);
+
+    return `You will have a ${performanceReview[randomPerformance]} performance`;
+};
+
+console.log(matchPerformance());
